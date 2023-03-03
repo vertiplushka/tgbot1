47c301c03f6337396c420a4b6ed4a06751e6cd47
vertiplushka <75839950+vertiplushka@users.noreply.github.com>
2023-03-04 03:03:35 +0700

    asdasdads

--- app.js
+++ app.js
@@ -250,7 +250,7 @@ const start = async () => {
         }
 })    
 }
-setTimeout(setInterval(() => {
+setTimeout(() => setInterval(() => {
     
     bufSC += startCounter
     bufWC += writeCounter
